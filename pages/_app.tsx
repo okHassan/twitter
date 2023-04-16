c8e2137f480d3584360b00e0584aa8737a449f83
Hassanahmed006 <freelancerhassan3@gmail.com>
2023-04-16 21:24:57 +0600

    test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,24 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Toaster } from 'react-hot-toast';
+import { SessionProvider } from 'next-auth/react';
+
+import Layout from '@/components/Layout'
+import '@/styles/globals.css'
+import EditModal from '@/components/modals/EditModal';
+import RegisterModal from '@/components/modals/RegisterModal';
+import LoginModal from '@/components/modals/LoginModal';
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <SessionProvider session={pageProps.session}>
+      <Toaster />
+      <RegisterModal />
+      <LoginModal />
+      <EditModal />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </SessionProvider>
+  )
 }
